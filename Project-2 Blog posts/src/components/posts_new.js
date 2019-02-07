acec9f9496893e3ea2d3e822420bfb0bbc5bf235
AbdullahBM <abdullah.m9404@outlook.com>
2019-02-07 22:32:24 +0500

    Redux and Blog Post form starting

---
+++ Project-2 Blog posts/src/components/posts_new.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+
+class PostsNew extends Component {
+  renderField(field) {
+    return (
+      <div className="form-group">
+        <label>{field.label}</label>
+        <input className="form-control" type="text" {...field.input} />
+      </div>
+    );
+  }
+
+  render() {
+    return (
+      <form>
+        <Field label="Post Title" name="title" component={this.renderField} />
+        <Field label="Tags" name="tag" component={this.renderField} />
+        <Field
+          label="Post Content"
+          name="content"
+          component={this.renderField}
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "PostNewForm"
+})(PostsNew);
